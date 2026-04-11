09b61bbfcf08c4390624f98b9acb6f288aac5dc9
Hugo Maia <maiahugom123@gmail.com>
2026-04-11 15:19:13 -0300

    day6 simple calculator

---
+++ week-01/day6.py
@@ -0,0 +1,8 @@
+# Simple calculator
+
+num1 = float(input("Enter the value: "))
+num2 = float(input("Enter the value: "))
+print(f"{num1} + {num2} = {num1+num2}")
+print(f"{num1} - {num2} = {num1-num2}")
+print(f"{num1} * {num2} = {num1*num2}")
+print(f"{num1} / {num2} = {num1/num2}")
