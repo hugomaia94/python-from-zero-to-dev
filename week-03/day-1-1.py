30a0982ada59c4b4d6930ab0c178c9c06ca65d2d
Hugo Maia <maiahugom123@gmail.com>
2026-04-22 15:10:47 -0300

    feat: create list and add elements using append

---
+++ week-03/day-1-1.py
@@ -0,0 +1,8 @@
+# Add Numbers → Create an empty list called numbers. Add the values 10, 5, 20, and 15 using append. Display the list.
+
+numbers = []
+numbers.append(10)
+numbers.append(5)
+numbers.append(20)
+numbers.append(15)
+print(numbers)
