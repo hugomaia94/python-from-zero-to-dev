d8b76302bb09774302a9bf3497de3978f910c565
Hugo Maia <maiahugom123@gmail.com>
2026-04-22 15:16:06 -0300

    feat: sort list in ascending and descending order

---
+++ week-03/day-1-3.py
@@ -0,0 +1,7 @@
+# Sort Numbers → Create a list [7, 3, 9, 1, 5]. Sort the list in ascending order, then sort it in descending order. Display both results.
+
+numbers = [7, 3, 9, 1, 5]
+numbers.sort()
+print(numbers)
+numbers.sort(reverse=True)
+print(numbers)
