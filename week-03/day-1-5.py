8941ef8edd440469b392e5f31f961a12c8f9db8f
Hugo Maia <maiahugom123@gmail.com>
2026-04-22 15:36:24 -0300

    feat: perform list operations with remove, append, and sort

---
+++ week-03/day-1-5.py
@@ -0,0 +1,6 @@
+# List Operations → Given the list `[8, 3, 8, 1, 9, 3]`, remove the first occurrence of the number 3. Add the number 10 using `append`. Sort the list and display the final result.
+numbers = [8, 3, 8, 1, 9, 3]
+numbers.remove(3)
+numbers.append(10)
+numbers.sort()
+print(numbers)
