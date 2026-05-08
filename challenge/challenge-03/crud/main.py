29bcb922f4a2321a1cc166308b248dd8effe60db
Hugo Maia <maiahugom123@gmail.com>
2026-05-08 00:56:51 -0300

    refactor: apply modularization, rename users to database and create main entry point

---
+++ challenge/challenge-03/crud/main.py
@@ -0,0 +1,20 @@
+import database as db
+
+while True:
+    print(
+        f" 1 - Add new user.\n 2 - List all users.\n 3 - Update a user.\n 4 - Delete a user.\n 5 - Exit."
+    )
+    option = input("Enter an opition: ")
+    if option == "1":
+        user = int(input("Enter the number of new users: "))
+
+        for i in range(user):
+            db.create_user()
+
+    elif option == "2":
+        all_users = db.get_all_users()
+
+        for user_id, dados in all_users.items():
+            print(
+                f"\n ID:{user_id}\n Name:{dados['name']}\n Age:{dados['age']}\n E_mail:{dados['e_mail']}\n____________\n"
+            )
