83517344181aee86389b0f54a291aab66ac315e1
Hugo Maia <maiahugom123@gmail.com>
2026-05-12 22:09:57 -0300

    refactor: reorganize week 03 structure and promote CRUD to main project

---
+++ challenge/crud-system/challenge-week-3.md
@@ -0,0 +1,17 @@
+# User Management System (CLI)
+
+A simple CRUD system built with Python.
+
+## Features
+
+- Create user
+- List users
+- Update user
+- Delete user
+
+## Technologies
+
+- Python
+- Dictionaries
+- Functions
+- Modularization
---
+++ challenge/crud-system/main.py
@@ -0,0 +1,29 @@
+import database as db
+
+while True:
+    print(
+        f" 1 - Add new user.\n 2 - List all users.\n 3 - Update a user.\n 4 - Delete a user.\n 5 - Exit."
+    )
+    option = input("Enter an opition: ")
+    if option == "1":
+        user = int(input("Enter the number of new users: "))
+
+        for i in range(user):
+            db.create_user()
+            print("\n")
+
+    elif option == "2":
+        all_users = db.get_all_users()
+
+        for user_id, dados in all_users.items():
+            print(
+                f"\n ID:{user_id}\n Name:{dados['name']}\n Age:{dados['age']}\n E_mail:{dados['e_mail']}\n____________\n"
+            )
+    elif option == "3":
+        db.update_user()
+    elif option == "4":
+        db.delete_user()
+    elif option == "5":
+        break
+    else:
+        print(f"\nPlease enter a valid option.\n")
